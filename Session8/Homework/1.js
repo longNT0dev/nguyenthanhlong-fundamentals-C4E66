5500aa0fdc85049259cc562153dd23b60a45cb83
unknown <codeinlife10@gamil.com                                   git config user.name codeinlife10>
2020-02-21 18:26:23 +0700

    initial commit

--- Session8/Homework/1.js
+++ Session8/Homework/1.js
@@ -13,7 +13,7 @@ const viewData = async ()=>{
     let list=document.getElementById("list")
     content.data.forEach((value,index) => {
         let html=`
-        <div id="block">
+        <div id="users-${index}" class="block">
             <div class="unview">${value.employee_salary}</div>
             <li id="user-${index}">Name: ${value.employee_name}</li>
             <li>Salary: ${value.employee_salary}</li>
@@ -24,17 +24,13 @@ const viewData = async ()=>{
     });
 }
 const view=async ()=>{
-      await viewData()
-      let a=document.getElementById("block")
-      for(let index = 0 ; index < content.data.length ; index++){
-    let userInfor=document.getElementById(`user-${index}`)
-    userInfor.addEventListener("mouseover",()=>{
+    await viewData()
+    for(let index = 0 ; index < content.data.length ; index++){
+        let a=document.getElementById(`users-${index}`)
+        let userInfor=document.getElementById(`user-${index}`)
+        userInfor.addEventListener("mouseover",()=>{
         a.children[0].classList.toggle("view")
     })
 }
 }
-let ad=document.getElementById("add")
-ad.addEventListener('click',()=>{
-    
-})
 view()
