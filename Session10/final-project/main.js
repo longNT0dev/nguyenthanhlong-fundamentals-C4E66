75740759683a87dbd363e02e81aabdf07a3f0cc4
codeinlife10 <codeinlife10@gamil.com                                   git config user.name codeinlife10>
2020-05-09 08:46:30 +0700

    completed

--- Session10/final-project/main.js
+++ Session10/final-project/main.js
@@ -1,38 +1,56 @@
+
 const list=document.getElementById("content-list")
 const form=document.getElementById("userData")
 const summit=document.getElementById("summit")
-var timer=0
+
 // realtime data
 db.collection('user').orderBy("time","desc").onSnapshot(snapshot=>{ //desc =>> stt giảm dần 
-    let changes=snapshot.docChanges() // detect change intime , la 1 array 
+    let changes=snapshot.docChanges() // detect change intime , la 1 array
+    console.log(changes)
     changes.forEach(change => {
         if(change.type=='added'){
             render(change.doc)
-        // }else if(change.type='updated'){
-        //     let olddiv=list.querySelector('[data-id=' + change.doc.id + ']')
-        //     list.removeChild(olddiv)
+        }else if(change.type=='modified'){
+            console.log(change.doc.data().like)
+            console.log(change.doc.data().dislike)
+            updateLikeData(change.doc.id,change.doc.data().like)
+            updateDislikeData(change.doc.id,change.doc.data().dislike)
         }
     });
 })
+
+function updateLikeData(id,dataLike){
+    let likecount = document.getElementById(id + " " + "like")
+    likecount.textContent = dataLike 
+}
+function updateDislikeData(id,dataDislike){
+    let dislikecount = document.getElementById(id + " " + "dislike")
+    dislikecount.textContent = dataDislike
+}
 //saving data
 form.addEventListener('submit',(e)=>{
     e.preventDefault() 
+    //realtime 
+    let today = new Date()
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+    let dateTime = date+' '+time
     db.collection("user").add({
         hastag:form.hastag.value,
         userName:form.userName.value,
         content:form.content.value,
         like:0,
-        time:timer,
+        dislike:0,
+        time:dateTime,
     })
     form.userName.value = ""
     form.hastag.value= ""
     form.content.value = ""
-    timer++
 })
+
 function render(doc){
     let posts=document.createElement('div')
     posts.setAttribute("class","content")
-    posts.setAttribute('id','posts')
 // image   
     let images=document.createElement('div')
     images.setAttribute("class","image-main")
@@ -43,11 +61,11 @@ function render(doc){
     image.setAttribute("class","image-class")
 
     images.appendChild(image)
-// content
+// include 3 
     let content=document.createElement('div')
-    // content.setAttribute("id",doc.id)
     content.setAttribute("class",'wrapper-info')
-    
+    content.setAttribute("id","doc.id")
+
 //hastag
     let hastag=document.createElement('p')
     hastag.textContent="#"+doc.data().hastag
@@ -71,61 +89,56 @@ function render(doc){
     content.appendChild(author)
     content.appendChild(post)
 //interact
-    
-
     let interact=document.createElement("div")
     interact.setAttribute("class","interact")
+
 //like
-let count1=0
     let likecount=document.createElement("p")
-    likecount.setAttribute("id","likecount")
-    likecount.innerHTML=count1
-   
+    likecount.setAttribute("id",doc.id + " " + "like")
+    likecount.textContent=doc.data().like
 //button like    
     let icon1=document.createElement("i")
     icon1.setAttribute("class","fas fa-thumbs-up")
 
     let button1=document.createElement("button")
     button1.setAttribute("class","btn-interact")
+    let count1 = doc.data().like
     button1.addEventListener("click",()=>{
-        db.collection("user").doc("b9pTMDpgsIe3Licwqttt").update({
-            like:count1
-        })
-        // likecount.innerHTML= doc.data().like++
-       
+        ++count1
+        console.log(count1)
+        db.collection('user').doc(`${doc.id}`).update({
+            like : count1
+        }) 
     })
-
+       
     button1.appendChild(icon1)
-   
 //dislike
-let count2=0
     let dislikecount=document.createElement("p")
-    dislikecount.innerHTML=count2
+    dislikecount.setAttribute("id",doc.id + " " + "dislike")
+    dislikecount.textContent=doc.data().dislike
 //button dislike   
     let icon2=document.createElement("i")
     icon2.setAttribute("class","fas fa-thumbs-down")
 
     let button2=document.createElement("button")
     button2.setAttribute("class","btn-interact")
+    let count2 = doc.data().dislike
     button2.addEventListener("click",()=>{
-        count2++
-        dislikecount.innerHTML=count2
+        ++count2
+        db.collection('user').doc(`${doc.id}`).update({
+            dislike : count2
+        })
     })
-
 
     button2.appendChild(icon2)
    
-//end
     interact.appendChild(likecount)
     interact.appendChild(button1)
     interact.appendChild(dislikecount)
     interact.appendChild(button2)
-    
+//end    
     posts.appendChild(images)
     posts.appendChild(content)
     posts.appendChild(interact)
     list.appendChild(posts)
 }
-summit.addEventListener("click",(doc)=>{
-    render(doc)
-})
